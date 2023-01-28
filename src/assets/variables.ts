17f4626fc13c1373649570596a60d94645308a20
Fearinn <matheusmgomes09@gmail.com>
2023-01-28 16:21:38 -0300

    css: more colors added

--- src/assets/variables.ts
+++ src/assets/variables.ts
@@ -1,1 +1,13 @@
-export const [primaryFontColor, secondaryFontColor, gradient] = ["#000000", "#fff", "linear-gradient(97.54deg, #002f52 35.49%, #326589 165.37%)"];
+export const [
+  primaryFontColor,
+  secondaryFontColor,
+  styledFontColor1,
+  styledFontColor2,
+  gradient,
+] = [
+  "#000000",
+  "#fff",
+  "#eb9b00",
+  "#002F52",
+  "linear-gradient(97.54deg, #002f52 35.49%, #326589 165.37%)",
+];
