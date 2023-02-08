3826faf38051a930f14e17e1462817c91ded1da2
Fearinn <matheusmgomes09@gmail.com>
2023-02-07 21:28:04 -0300

    feat: AbModal component added to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,38 @@
+import { AbModal } from "alurabooks-ds-fearinn";
+import { useState } from "react";
 import Categories from "./Categories";
 import { StyledHeader, StyledLogin } from "./StyledHeader";
 
 function Header() {
+  const [modal, setModal] = useState(false);
+  
+  function closeModal() {
+    setModal(false)
+  }
+
   return (
-    <StyledHeader>
-      <div className="logo">
-        <div className="logo-image" />
-        <h1>
-          <span>Alura</span>Books
-        </h1>
-      </div>
-      <Categories />
-      <nav className="user-navigation">
-        <StyledLogin type="button" className="login">
-          <div className="login-bg" />
-          <span>Login</span>
-        </StyledLogin>
-      </nav>
-    </StyledHeader>
+    <>
+      <StyledHeader>
+        <div className="logo">
+          <div className="logo-image" />
+          <h1>
+            <span>Alura</span>Books
+          </h1>
+        </div>
+        <Categories />
+        <nav className="user-navigation">
+          <StyledLogin
+            type="button"
+            className="login"
+            onClick={() => setModal(true)}
+          >
+            <div className="login-bg" />
+            <span>Login</span>
+          </StyledLogin>
+        </nav>
+      </StyledHeader>
+      <AbModal title="login" whenClosed={closeModal} htmlId="abmodal-login-id" open={modal}/>
+    </>
   );
 }
 
