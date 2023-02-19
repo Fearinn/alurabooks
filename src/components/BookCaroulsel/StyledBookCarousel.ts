10a648e765ab90b60c110eee79cb51ba8c7b1d54
Fearinn <matheusmgomes09@gmail.com>
2023-02-19 12:07:13 -0300

    css: scroll added to carousel overflow

--- src/components/BookCaroulsel/StyledBookCarousel.ts
+++ src/components/BookCaroulsel/StyledBookCarousel.ts
@@ -4,20 +4,27 @@ export const StyledBookCarousel = styled.div`
   display: flex;
   justify-content: center;
   gap: 1rem;
-  overflow-x: hidden;
+  max-width: 100%;
+  overflow-x: scroll;
 
   &:has(.book-cover:active) {
     .book-cover {
-        opacity: 0.9;
+      opacity: 0.9;
     }
   }
 `;
 
 export const StyledBookCover = styled.div<{ selected: boolean }>`
   cursor: pointer;
+  width: 40%;
+  flex-shrink: 0;
+
+  img {
+    width: 100%;
+  }
 
   @media screen and (min-width: 1024px) {
-    ${({selected}) => !selected && "transform: scale(0.7)"}
+    width: auto;
+    ${({ selected }) => !selected && "transform: scale(0.7)"}
   }
 `;
-
