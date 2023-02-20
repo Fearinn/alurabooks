ee61c33a7a8796096254e33cd015850b2232927e
Fearinn <matheusmgomes09@gmail.com>
2023-02-19 21:52:17 -0300

    feat: Loader component added

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,14 @@
+import StyledLoader from "./StyledLoader";
+
+function Loader() {
+  return (
+    <StyledLoader>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </StyledLoader>
+  );
+}
+
+export default Loader;
